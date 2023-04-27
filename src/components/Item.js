156b29d51e4855d9d92ad08be2031980bc2ae433
Jesse <jessemutua76@gmail.com>
2023-04-27 22:26:21 +0300

    Assignmnet

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,38 @@
 import React from "react";
 
-function Item({ item }) {
-  return (
-    <li className={item.isInCart ? "in-cart" : ""}>
-      <span>{item.name}</span>
-      <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
-        {item.isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-      <button className="remove">Delete</button>
-    </li>
-  );
+function Item({ item, onUpdateItem }) {
+
+    function handleAddToCartClick() {
+        fetch(`http://localhost:4000/items/${item.id}`, {
+                method: "PATCH",
+                headers: {
+                    "content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    isInCart: !item.isInCart,
+                }),
+            })
+            .then((r) => r.json())
+            .then((updatedItem) => onUpdateItem(updatedItem))
+    }
+
+    function handleDeleteClick() {
+        console.log(item);
+    }
+
+    return ( <
+        li className = { item.isInCart ? "in-cart" : "" } >
+        <
+        span > { item.name } < /span> <
+        span className = "category" > { item.category } < /span> <
+        button className = { item.isInCart ? "remove" : "add" }
+        onClick = { handleAddToCartClick } > { item.isInCart ? "Remove From" : "Add to" }
+        Cart <
+        /button> <
+        button className = "remove"
+        onClick = { handleDeleteClick } > Delete < /button> <
+        /li>
+    );
 }
 
 export default Item;
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,37 +1,61 @@
 import React, { useState } from "react";
 
-function ItemForm() {
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce");
+function ItemForm({ onAddItem }) {
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("Produce");
 
-  return (
-    <form className="NewItem">
-      <label>
+    function handleSubmit(e) {
+        e.preventDefault();
+        const itemData = {
+            name: name,
+            category: category,
+            isInCart: false,
+        };
+        fetch(' http://localhost:4000/items', {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(itemData),
+            })
+            .then((r) => r.json())
+            .then((newItem) => onAddItem(newItem))
+    }
+
+    return ( <
+        form className = "NewItem" >
+        <
+        label >
         Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
+        <
+        input type = "text"
+        name = "name"
+        value = { name }
+        onChange = {
+            (e) => setName(e.target.value) }
+        /> <
+        /label>
 
-      <label>
+        <
+        label >
         Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
+        <
+        select name = "category"
+        value = { category }
+        onChange = {
+            (e) => setCategory(e.target.value) } >
+        <
+        option value = "Produce" > Produce < /option> <
+        option value = "Dairy" > Dairy < /option> <
+        option value = "Dessert" > Dessert < /option> <
+        /select> <
+        /label>
 
-      <button type="submit">Add to List</button>
-    </form>
-  );
+        <
+        button type = "submit"
+        onClick = { handleSubmit } > Add to List < /button> <
+        /form>
+    );
 }
 
 export default ItemForm;
